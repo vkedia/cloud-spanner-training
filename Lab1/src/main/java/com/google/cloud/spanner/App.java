6ca596bec1ec6cde136ffdf7c50408143f502d19
Vikas Kedia <vikas.kedia@gmail.com>
2017-06-30 16:21:05 -0700

    Refactoring

--- Lab1/src/main/java/com/google/cloud/spanner/App.java
+++ Lab1/src/main/java/com/google/cloud/spanner/App.java
@@ -10,21 +10,21 @@ public class App {
   private static final String INSTANCE_ID = "test-instance";
   private static final String DATABASE_ID = "example-db";
 
-  static double getAccountBalance(long accountId) {
+  static double getAccountBalance(DatabaseClient client, long accountId) {
     return 0;
   }
 
-  static double getCustomerBalance(long customerId) {
+  static double getCustomerBalance(DatabaseClient client, long customerId) {
     return 0;
   }
 
-  static void depositMoney(long accountId, double amount) {
+  static void depositMoney(DatabaseClient client, long accountId, double amount) {
   }
 
   /**
    * Returns false if money could not be withdrawn due to insufficient balance.
    */
-  static boolean withdrawMoney(long accountId, double amount) {
+  static boolean withdrawMoney(DatabaseClient client, long accountId, double amount) {
     return false;
   }
 
@@ -42,26 +42,26 @@ public class App {
     Spanner spanner = options.getService();
     try {
       DatabaseId db = DatabaseId.of(options.getProjectId(), INSTANCE_ID, DATABASE_ID);
-      DatabaseClient dbClient = spanner.getDatabaseClient(db);
+      DatabaseClient client = spanner.getDatabaseClient(db);
       String command = args[0];
       switch (command) {
         case "account-balance":
           long accountId = Long.parseLong(args[1]);
-          System.out.println(getAccountBalance(accountId));
+          System.out.println(getAccountBalance(client, accountId));
           break;
         case "customer-balance":
           long customerId = Long.parseLong(args[1]);
-          System.out.println(getCustomerBalance(customerId));
+          System.out.println(getCustomerBalance(client, customerId));
           break;
         case "deposit":
           accountId = Long.parseLong(args[1]);
           double amount = Double.parseDouble(args[2]);
-          depositMoney(accountId, amount);
+          depositMoney(client, accountId, amount);
           break;
         case "withdraw":
           accountId = Long.parseLong(args[1]);
           amount = Double.parseDouble(args[2]);
-          boolean success = withdrawMoney(accountId, amount);
+          boolean success = withdrawMoney(client, accountId, amount);
           if (!success) {
             System.out.println("Insufficient balance");
           }
